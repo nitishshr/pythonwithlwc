d90df5ba56666e3bfb254d2b1186ebe7b7717d76
nitishshr <nitishshr48@gmail.com>
2020-05-22 00:55:09 +0530

    adding image url supporyt for front end

--- shop.py
+++ shop.py
@@ -4,13 +4,15 @@ clothesCollection = {
         "color": "Blue",
         "name": "Ripped blue jeans",
         "price": 1000,
-        "type": "Ripped"
+        "type": "Ripped",
+        "url" :"https://my-shop.online/wp-content/uploads/2018/12/HTB18ydSIf5TBuNjSspcq6znGFXaJ.jpg"
       },
       {
         "color": "black",
         "name": "black jeans",
         "price": 2000,
-        "type": "vintage"
+        "type": "vintage",
+        "url" :"https://images-na.ssl-images-amazon.com/images/I/61Cd43CJ6FL._UY606_.jpg"
       }
     ],
     "T_Shirts": [
@@ -18,19 +20,22 @@ clothesCollection = {
         "color": "blue",
         "name": "Blue V neck T-shirt",
         "price": 500,
-        "type": "V"
+        "type": "V",
+        "url":"https://5.imimg.com/data5/XX/WI/MY-30100598/men-s-v-neck-t-shirt-500x500.jpg"
       },
       {
         "color": "Red",
         "name": "Red V neck T-shirt",
         "price": 1000,
-        "type": "V"
+        "type": "V",
+        "url":"https://cdn.shopify.com/s/files/1/1220/2068/products/red_vneck_grande.jpg?v=1556824301"
       },
       {
         "color": "blue",
         "name": "Blue polo T-shirt",
         "price": 500,
-        "type": "polo"
+        "type": "polo",
+        "url":"https://static.cilory.com/315408-thickbox_default/peter-england-royal-blue-polo-t-shirt.jpg"
       }
     ],
     "shirts": [
@@ -38,19 +43,22 @@ clothesCollection = {
         "color": "blue",
         "name": "Blue formal shirt",
         "price": 500,
-        "type": "formal"
+        "type": "formal",
+        "url" :"https://rukminim1.flixcart.com/image/714/857/shirt/s/h/y/46-bfrybluesht02-being-fab-original-imaekjr8ymhnxznp.jpeg?q=50"
       },
       {
         "color": "Red",
         "name": "Red formal shirt",
         "price": 1000,
-        "type": "formal"
+        "type": "formal",
+        "url" :"https://images-na.ssl-images-amazon.com/images/I/61FFyFD1coL._UY550_.jpg"
       },
       {
         "color": "Pink",
         "name": "Pink casual shirt",
         "price": 1500,
-        "type": "casual"
+        "type": "casual",
+        "url" :"https://img0.junaroad.com/uiproducts/11951226/zoom_0-1475748755.jpg"
       }
     ]
   }
