09d7b68e72bf1c60773fdfc6a0e693ec759a83c6
nitishshr <nitishshr48@gmail.com>
2020-05-23 00:03:27 +0530

    added extId for salesforce

--- shop.py
+++ shop.py
@@ -5,14 +5,16 @@ clothesCollection = {
         "name": "Ripped blue jeans",
         "price": 1000,
         "type": "Ripped",
-        "url" :"https://my-shop.online/wp-content/uploads/2018/12/HTB18ydSIf5TBuNjSspcq6znGFXaJ.jpg"
+        "url" :"https://my-shop.online/wp-content/uploads/2018/12/HTB18ydSIf5TBuNjSspcq6znGFXaJ.jpg",
+        "extId":"jean1"
       },
       {
         "color": "black",
         "name": "black jeans",
         "price": 2000,
         "type": "vintage",
-        "url" :"https://images-na.ssl-images-amazon.com/images/I/61Cd43CJ6FL._UY606_.jpg"
+        "url" :"https://images-na.ssl-images-amazon.com/images/I/61Cd43CJ6FL._UY606_.jpg",
+        "extId":"jean2"
       }
     ],
     "T_Shirts": [
@@ -21,21 +23,24 @@ clothesCollection = {
         "name": "Blue V neck T-shirt",
         "price": 500,
         "type": "V",
-        "url":"https://5.imimg.com/data5/XX/WI/MY-30100598/men-s-v-neck-t-shirt-500x500.jpg"
+        "url":"https://5.imimg.com/data5/XX/WI/MY-30100598/men-s-v-neck-t-shirt-500x500.jpg",
+        "extId":"tshirt1"
       },
       {
         "color": "Red",
         "name": "Red V neck T-shirt",
         "price": 1000,
         "type": "V",
-        "url":"https://cdn.shopify.com/s/files/1/1220/2068/products/red_vneck_grande.jpg?v=1556824301"
+        "url":"https://cdn.shopify.com/s/files/1/1220/2068/products/red_vneck_grande.jpg?v=1556824301",
+        "extId":"tshirt2"
       },
       {
         "color": "blue",
         "name": "Blue polo T-shirt",
         "price": 500,
         "type": "polo",
-        "url":"https://static.cilory.com/315408-thickbox_default/peter-england-royal-blue-polo-t-shirt.jpg"
+        "url":"https://static.cilory.com/315408-thickbox_default/peter-england-royal-blue-polo-t-shirt.jpg",
+        "extId":"tshirt3"
       }
     ],
     "shirts": [
@@ -44,21 +49,24 @@ clothesCollection = {
         "name": "Blue formal shirt",
         "price": 500,
         "type": "formal",
-        "url" :"https://rukminim1.flixcart.com/image/714/857/shirt/s/h/y/46-bfrybluesht02-being-fab-original-imaekjr8ymhnxznp.jpeg?q=50"
+        "url" :"https://rukminim1.flixcart.com/image/714/857/shirt/s/h/y/46-bfrybluesht02-being-fab-original-imaekjr8ymhnxznp.jpeg?q=50",
+        "extId":"shirt1"
       },
       {
         "color": "Red",
         "name": "Red formal shirt",
         "price": 1000,
         "type": "formal",
-        "url" :"https://images-na.ssl-images-amazon.com/images/I/61FFyFD1coL._UY550_.jpg"
+        "url" :"https://images-na.ssl-images-amazon.com/images/I/61FFyFD1coL._UY550_.jpg",
+        "extId":"shirt2"
       },
       {
         "color": "Pink",
         "name": "Pink casual shirt",
         "price": 1500,
         "type": "casual",
-        "url" :"https://img0.junaroad.com/uiproducts/11951226/zoom_0-1475748755.jpg"
+        "url" :"https://img0.junaroad.com/uiproducts/11951226/zoom_0-1475748755.jpg",
+        "extId":"shirt3"
       }
     ]
   }
